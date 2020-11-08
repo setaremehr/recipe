8476214279de92584602590eff0036a9b306216c
setare mehr <setaremehr89@gmail.com>
2020-11-08 01:46:17 -0800

    added home background

--- client/src/pages/mainPage/mainPage.js
+++ client/src/pages/mainPage/mainPage.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+// import Media from "react-bootstrap/Media";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import  BackImage from './background';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+
+// import Image from 'react-bootstrap/Image'
+function MainPage() {
+  return (
+    
+  
+  <div className="bg">
+
+ <BackImage />
+  </div>
 
 
-function MainPage () {
-    return(
-<div>
 
-</div>
-    );
+       
+
+  );
 }
 
 export default MainPage;
